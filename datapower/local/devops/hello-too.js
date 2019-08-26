fa1f94794153a5ec1adc42473ddeb26d81a071c5
Tyler Nelson <tyler.nelson@erieinsurance.com>
2019-08-26 12:29:43 -0400

    Add dp-devops specifics

--- datapower/local/devops/hello-too.js
+++ datapower/local/devops/hello-too.js
@@ -1,6 +1,6 @@
 var hm = require('header-metadata');
 
-// Set the X-Hello-World header 
+// Set the X-Hello-World header
 hm.current.set('X-Hello-World', 'Hello from DataPower domain foo');
 
 session.input.readAsBuffer (function (error, buffer) {
@@ -10,6 +10,5 @@ session.input.readAsBuffer (function (error, buffer) {
     }
     // Since this simple application only returns a test hello world
     // string, we'll just prepend our placeholder string
-    session.output.write("DataPower Proxied: " + buffer.toString());
+    session.output.write("DataPower devops Proxied: " + buffer.toString());
 });
-
